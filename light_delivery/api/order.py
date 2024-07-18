f1cccc3aca73536599ea66d00d89c2f64d8d0744
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-07-19 02:22:31 +0300

    uuuu

--- light_delivery/api/order.py
+++ light_delivery/api/order.py
@@ -149,10 +149,16 @@ def get_zone_address():
 		return res
 
 
-from frappe.utils import get_request_header
 
 def validate_token():
-    token = get_request_header("Authorization", "").split("Bearer ")[-1]
+    # Get the Authorization header
+    auth_header = frappe.local.request.headers.get("Authorization")
+    
+    if not auth_header or not auth_header.startswith("Bearer "):
+        frappe.throw(_("Missing or invalid token"), frappe.AuthenticationError)
+    
+    # Extract the token from the Authorization header
+    token = auth_header.split("Bearer ")[-1]
     
     if not token:
         frappe.throw(_("Missing token"), frappe.AuthenticationError)
@@ -160,8 +166,8 @@ def validate_token():
     # Assuming you are using JWT for tokens
     try:
         decoded_token = frappe.jwt.decode(token, frappe.conf.secret)
-    except Exception:
-        frappe.throw(_("Invalid token"), frappe.AuthenticationError)
+    except Exception as e:
+        frappe.throw(_("Invalid token: {0}").format(str(e)), frappe.AuthenticationError)
     
     user = decoded_token.get("user")
     
@@ -175,3 +181,4 @@ def validate_token():
     
     # Set the user for the current request
     frappe.set_user(user)
+
