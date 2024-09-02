cc367027271f2e0e75e22ac3814a5b9c1bb58066
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-09-02 06:40:55 +0300

    create new feature to draw the road of order

--- light_delivery/light_delivery/doctype/order/order.py
+++ light_delivery/light_delivery/doctype/order/order.py
@@ -1,9 +1,36 @@
 # Copyright (c) 2024, Muhammad Essam and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
+import json
 
 
 class Order(Document):
-	pass
+	def on_change(self):
+		self.draw_roads()
+
+	def draw_roads(self):
+		
+		if self.status in ['Returned' , 'Delivered']:
+			road = self.get('road')
+			coord = []
+			if road:
+				for i in road:
+					coord.append([float(i.pointer_x),float(i.pointer_y)])
+				coordinates = {
+					"type":"FeatureCollection",
+					"features":[
+						{
+							"type":"Feature",
+							"properties":{},
+							"geometry":{
+								"type":"LineString",
+								"coordinates":coord
+							}
+						}
+					]
+				}
+				self.road_map = json.dumps(coordinates)
+				frappe.db.commit()
