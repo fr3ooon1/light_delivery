4ce7f43956c14bf5065a3152b16c4ecdcdc06d80
fr3ooon1 <fero>
2024-09-03 18:09:12 +0300

    fix

--- light_delivery/light_delivery/doctype/request_delivery/request_delivery.py
+++ light_delivery/light_delivery/doctype/request_delivery/request_delivery.py
@@ -24,11 +24,13 @@ class RequestDelivery(Document):
 		self.number_of_order = len(order_request)
 		store = frappe.get_doc("Store",self.store)
 		store_discount = store.get('store_discount')
-		if not store_discount:
-			return False
+		# if not store_discount:
+		# 	return False
 		for i in range(len(order_request)):
 			order = frappe.get_doc("Order" , self.get('order_request')[i].get("order"))
-			order.discount = store_discount[i].get("discount")
+			order.discount = store_discount[i].get("discount") if store_discount else 0
+
+			self.order_request[i] = self.delivery
 
 			order.delivery = order_request[i].get("delivery")
 			order.store = order_request[i].get("store")
