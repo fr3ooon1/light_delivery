edbcadc9e18b79435a316e6613bd679636ee5ee9
fr3ooon1 <fero>
2024-10-10 11:34:30 +0300

    update

--- light_delivery/light_delivery/doctype/store/store.py
+++ light_delivery/light_delivery/doctype/store/store.py
@@ -1,12 +1,31 @@
 # Copyright (c) 2024, Muhammad Essam and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+import json
 
 
 class Store(Document):
-	pass
-
-
-
+	def validate(self):
+		self.draw_location()
+		self.rate_delivery()
+	
+	def draw_location(self):
+		if self.pointer_x and self.pointer_y:
+			coordi = [float(self.pointer_x) , float(self.pointer_y)]
+			coordinates = {
+					"type":"FeatureCollection",
+					"features":[
+						{
+							"type":"Feature",
+							"properties":{},
+							"geometry":{
+								"type":"Point",
+								"coordinates":coordi
+							}
+						}
+					]
+				}
+			self.delivery_location = json.dumps(coordinates)
+			frappe.db.commit()
