6a0571193e5744231100e5c46e6e7a7b0107e5a2
fr3ooon1 <fero>
2024-10-10 12:45:08 +0300

    update

--- light_delivery/api/login.py
+++ light_delivery/api/login.py
@@ -158,7 +158,7 @@ def registration (*args , **kwargs):
 		frappe.db.commit()
 
 		contact = frappe.new_doc('Contact')
-		contact.first_name = store_obj.store_name if store_obj else delivery_obj.full_name
+		contact.first_name = store_obj.store_name if store_obj else delivery_obj.delivery_name
 		contact.append('phone_nos',{
 			"phone":kwargs.get('phone'),
 			"is_primary_mobile_no":1
@@ -166,7 +166,7 @@ def registration (*args , **kwargs):
 		contact.append('links',{
 			"link_doctype":"Store",
 			"link_name":store_obj.name if store_obj else delivery_obj.name , 
-			"link_type":store_obj.store_name if store_obj else delivery_obj.full_name
+			"link_type":store_obj.store_name if store_obj else delivery_obj.delivery_name
 		})
 		contact.append('email_ids',{
 			"email_id":kwargs.get("email"),
