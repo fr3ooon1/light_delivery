67ae77dc247a101211d37731081a9ea8f8d097d8
fr3ooon1 <fero>
2024-11-03 11:02:35 +0200

    update

--- light_delivery/api/consumer.py
+++ light_delivery/api/consumer.py
@@ -1,13 +1,13 @@
 import frappe 
 from frappe import _
 
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist(allow_guest=False)
 def get_stores(*args,**kwargs):
     pass
 
 
 
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist(allow_guest=False)
 def get_favorite(*args,**kwargs):
     res = []
     user = frappe.get_value("User",frappe.session.user,["username","full_name","mobile_no"],as_dict=True)
@@ -16,6 +16,8 @@ def get_favorite(*args,**kwargs):
         for store in stores:
             doc = frappe.get_doc("Store",store.get("store"))
             res.append({
+                "id":doc.name,
+                "is_favorite":is_favorite(user.get("username") , store.get("store")),
                 "cover":doc.store_cover,
                 "logo":doc.store_logo,
                 "store_name":frappe.get_value("User",doc.user,"full_name"),
@@ -35,31 +37,70 @@ def get_favorite(*args,**kwargs):
 
 
 
-@frappe.whitelist(allow_guest=True)
-def add_to_favorite(*args,**kwargs):
-    user = frappe.get_value("User",frappe.session.user,["username","full_name"],as_dict=True)
+@frappe.whitelist(allow_guest=False)
+def add_to_favorite(*args, **kwargs):
+    user = frappe.get_value("User", frappe.session.user, ["username", "full_name"], as_dict=True)
     store = kwargs.get("store")
-    if frappe.db.exists("Favorite",user.get("username")):
-        doc = frappe.get_doc("Favorite",user.get("username"))
-        doc.append("stores",{
-            "store":store
-        })
+    if not store:
+        frappe.local.response['http_status_code'] = 400
+        frappe.local.response['message'] = "Store is required."
+        return
+    
+    if frappe.db.exists("Favorite", user.get("username")):
+        doc = frappe.get_doc("Favorite", user.get("username"))
+        fav_stores = [row.store for row in doc.stores]
+        
+        if len(fav_stores) >= 10 and store not in fav_stores:
+            frappe.local.response['http_status_code'] = 400
+            frappe.local.response['message'] = "Maximum favorite stores limit reached."
+            return
+
+        if store in fav_stores:
+            doc.stores = [row for row in doc.stores if row.store != store]
+            doc.save(ignore_permissions=True)
+            frappe.db.commit()
+            frappe.local.response['http_status_code'] = 200
+            frappe.local.response['message'] = {
+                "message": f"Store {store} removed from favorite",
+                "data": False
+            }
+            return
+        
+        doc.append("stores", {"store": store})
         doc.save(ignore_permissions=True)
         frappe.db.commit()
-        
+        frappe.local.response['http_status_code'] = 200
+        frappe.local.response['message'] = {
+            "message": f"Store {store} added to favorite",
+            "data": True
+        }
+        return
+
     else:
         fav = frappe.new_doc("Favorite")
         fav.customer = user.get("username")
-        fav.append("stores",{
-            "store":store
-        })
+        fav.append("stores", {"store": store})
         fav.insert(ignore_permissions=True)
         frappe.db.commit()
-    
-    return f"""Store {store} added to the favorite"""
+        frappe.local.response['http_status_code'] = 200
+        frappe.local.response['message'] = {
+            "message": f"Store {store} added to favorite",
+            "data": True
+        }
 
 
-@frappe.whitelist(allow_guest=True)
+
+@frappe.whitelist(allow_guest=False)
 def search_for_store(*args,**kwargs):
     res = frappe.get_list("Store",{""})
     pass
+
+
+def is_favorite(customer , store):
+    fav_stores = frappe.get_list("Stores",{"parent":customer},pluck='store',ignore_permissions=True)
+    if store in fav_stores:
+        return True
+    else:
+        return False
+
+
