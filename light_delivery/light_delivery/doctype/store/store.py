0a738f27f8b779b6aaab01018e8b4ecb3c47f228
fr3ooon1 <mohamed.essam68.me@gmail.com>
2024-12-22 12:09:30 +0200

    update

--- light_delivery/light_delivery/doctype/store/store.py
+++ light_delivery/light_delivery/doctype/store/store.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 import json
 
@@ -9,6 +10,7 @@ import json
 class Store(Document):
 	def validate(self):
 		self.draw_location()
+		self.validate_price_list()
 	
 	def draw_location(self):
 		if self.pointer_x and self.pointer_y:
@@ -28,3 +30,9 @@ class Store(Document):
 				}
 			self.store_location = json.dumps(coordinates)
 			frappe.db.commit()
+
+	def validate_price_list(self):
+		if self.status == 'Active':
+			if self.minimum_price == 0 or self.rate_of_km == 0:
+				frappe.throw(_("Please select price list"))
+		
