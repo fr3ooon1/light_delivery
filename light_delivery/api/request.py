3788105a6f72285372544783ffe9ceae82276e6d
admin <ad@gmail.com>
2025-06-04 14:42:32 +0300

    update

--- light_delivery/api/request.py
+++ light_delivery/api/request.py
@@ -477,7 +477,7 @@ def get_request_details_for_del(*args, **kwargs):
 			JOIN `tabRequest Delivery` as rd ON rd.name = '{request_name}'
 			JOIN `tabOrder Request` as orq ON orq.parent = rd.name AND orq.order = o.name
 			WHERE rd.name = '{request_name}'
-			AND o.status NOT IN ('Pending','Store Cancel','Delivery Cancel','Cancel','Return to store') ;
+			AND o.status NOT IN ('Pending','Store Cancel','Delivery Cancel','Cancel','Return to store','Delivered') ;
 		""", as_dict=1)
 		
 		for i in order:
